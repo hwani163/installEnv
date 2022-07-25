f4cbd6739ba905b604d6cec4d93c565448cae382
Seokhwan Won <hwani163@naver.com>
2022-07-25 11:03:38 +0900

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,18 @@
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew install zsh
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+# zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+echo -e "\nZSH_THEME="robbyrussell"" >> ~/.zshrc
+eco -e "\nplugins=(" >> ~/.zshrc
+eco -e "\n  git" >> ~/.zshrc
+eco -e "\n  zsh-syntax-highlighting" >> ~/.zshrc
+eco -e "\n  zsh-autosuggestions" >> ~/.zshrc
+eco -e "\n)" >> ~/.zshrc
+
+source ~/.zshrc
