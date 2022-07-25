86c6d8e6af963353c8c1bfd20804b96f4c6a8c1c
Seokhwan Won <hwani163@naver.com>
2022-07-25 11:20:56 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,13 +3,13 @@ sudo apt install gitless
 sudo apt install zsh
 rm -rf ~/.oh-my-zsh
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-
+&&
 # zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-
+&&
 # zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-
+&&
 echo "\nZSH_THEME="robbyrussell"" >> ~/.zshrc
 echo "\nplugins=(" >> ~/.zshrc
 echo "\n  git" >> ~/.zshrc
