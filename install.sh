06e041689248f2eb129d75fd359babb35c7b426b
Seokhwan Won <hwani163@naver.com>
2022-07-25 11:07:39 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
+sudo apt install gitless
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew install zsh
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
