f7f8717f84de78d3359964603d189b5fb3d79e44
Seokhwan Won <hwani163@naver.com>
2022-07-25 11:14:54 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
+sudo apt update
 sudo apt install gitless
 sudo apt install zsh
-brew install zsh
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 # zsh-syntax-highlighting
